a6013125d4a121edac3d5288684940e00f1f7af9
unknown <yenertuz@hotmail.com>
2018-07-09 11:19:19 -0700

    m

--- index.php
+++ index.php
@@ -6,5 +6,6 @@
 <form method="post" action="login.php">
 Username: <input type="text" name="username"> <br>
 Password: <input type="password" name="password"> <br>
-<input type="submit" name="submit" value="Login">
+<input type="submit" name="submit" value="Login"> <br>
+<input type="submit" name="guest" value="Continue As Guest">
 </html>
